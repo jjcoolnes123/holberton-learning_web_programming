8e06986f9230c729254c3c1acddd304d54fa9f7d
jjcoolnes123 <jordannixon0026@gmail.com>
2019-08-09 08:45:39 -0400

    final project

---
+++ 5-javascript/demo_parameter.js
@@ -0,0 +1,10 @@
+// Store a value passed by the user to the script in the variable my_parameter. Note it takes the second element which corresponds to the first parameter. Third element would correspond to second parameter
+let my_parameter = process.argv[2];
+
+// Print the my_parameter variable
+console.log(my_parameter)
+Omar-MacBook-Pro:6-javascript_continued sylvain$ node demo_parameter.js Hello
+Hello
+Omar-MacBook-Pro:6-javascript_continued sylvain$
+
+last login: Friday Aug 2 3:55pm
